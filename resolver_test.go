658c2421a7d36c29a9f569dbdabf177db89bd308
Johan Lindh <johan@linkdata.se>
2025-09-24 09:12:39 +0200

    wip

--- resolver_test.go
+++ resolver_test.go
@@ -67,19 +67,6 @@ func Test_A_console_aws_amazon_com(t *testing.T) {
 	if !haveA {
 		t.Fatalf("missing A record terminating chain at %s", chainOwner)
 	}
-	/*
-		Expect the ANSWER section of the result to be like this. Actual names and IP's may differ,
-		but the chain of CNAMEs must be intact and it must contain at least one A record.
-
-		console.aws.amazon.com.	7191	IN	CNAME	console.cname-proxy.amazon.com.
-		console.cname-proxy.amazon.com.	51 IN	CNAME	lbr.us.console.amazonaws.com.
-		lbr.us.console.amazonaws.com. 51 IN	CNAME	eu-north-1.console.aws.amazon.com.
-		eu-north-1.console.aws.amazon.com. 7191	IN CNAME eu-north-1.console.cname-proxy.amazon.com.
-		eu-north-1.console.cname-proxy.amazon.com. 51 IN CNAME gr.aga.console-geo.eu-north-1.amazonaws.com.
-		gr.aga.console-geo.eu-north-1.amazonaws.com. 51	IN CNAME aba8735d2c3d241de.awsglobalaccelerator.com.
-		aba8735d2c3d241de.awsglobalaccelerator.com. 291	IN A 166.117.166.206
-		aba8735d2c3d241de.awsglobalaccelerator.com. 291	IN A 166.117.98.246
-	*/
 }
 
 func Test_TXT_qnamemintest_internet_nl(t *testing.T) {
@@ -92,10 +79,18 @@ func Test_TXT_qnamemintest_internet_nl(t *testing.T) {
 		t.Error(dns.RcodeToString[x])
 	}
 	if x := len(result.Answers); x < 1 {
-		t.Error(x)
+		t.Fatal(x)
 	}
-
-	/*
-		The result must have a TXT record that starts with "HOORAY".
-	*/
+	found := false
+	for _, rr := range result.Answers {
+		if rr, ok := rr.(*dns.TXT); ok {
+			for _, txt := range rr.Txt {
+				found = found || strings.HasPrefix(txt, "HOORAY")
+			}
+		}
+	}
+	if !found {
+		t.Error("expected a TXT record starting with HOORAY")
+		t.Log(result.Answers)
+	}
 }
